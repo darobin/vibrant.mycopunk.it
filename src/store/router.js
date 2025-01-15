5207c0fc75f0b59bbf2bd1202a902b9662a98587
Robin Berjon <robin.berjon@nytimes.com>
2025-01-15 15:15:00 +0100

    try to keep the dev stuff working

--- src/store/router.js
+++ src/store/router.js
@@ -1,6 +1,7 @@
 
 import { $isLoggedIn } from "./identity.js";
 import { createRouter, redirectPage } from "@nanostores/router";
+import isProd from "../lib/is-prod.js";
 
 export const $router = createRouter(
   {
@@ -14,7 +15,13 @@ export const $router = createRouter(
   }
 );
 
-$isLoggedIn.subscribe((val) => {
-  if (!val) return redirectPage($router, 'login');
-  return redirectPage($router, 'home');
-});
+// This is for dev, should stay at the top. Redirect to the dev site right away.
+if (isProd && window.location.search === '?dev') {
+  window.location = `https://vibrant.bast/${window.location.hash}`;
+}
+else {
+  $isLoggedIn.subscribe((val) => {
+    if (!val) return redirectPage($router, 'login');
+    return redirectPage($router, 'home');
+  });
+}
